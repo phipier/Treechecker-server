8db5a9d77a73c809835b060caee026647b0634c2
Pierric de Laborie <Pierric.deLaborie@ext.ec.europa.eu>
2019-08-08 17:58:09 +0200

    added Django cors headers

--- canhemon/settings.py
+++ canhemon/settings.py
@@ -31,6 +31,7 @@ else:
     ALLOWED_HOSTS = ['treechecker.pythonanywhere.com']
 
 
+
 # set models ordering
 """
 ADMIN_ORDERING = [
@@ -51,12 +52,14 @@ INSTALLED_APPS = [
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'rest_framework',
-	'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -233,6 +236,9 @@ LOGGING = {
         },
     }
 }
+
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 #try:
 #    from local_settings import *
